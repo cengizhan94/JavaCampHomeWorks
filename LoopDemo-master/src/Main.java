7335fcd390688c71a8000f7fd80f0df0201da959
Cengiz Han <90911173+cengizhan94@users.noreply.github.com>
2021-11-15 08:44:04 +0300

    Ders13
    LoopDemo

---
+++ LoopDemo-master/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("For Döngüsü Bitti");
+        System.out.println("**********************");
+        int i=0;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("While Döngüsü Bitti");
+
+        int j = 0;
+        do {
+           System.out.println(j);
+           j++;
+        } while (j <= 10) ;
+        System.out.println("Do While Döngüsü Bitti");
+    }
+
+
+}
