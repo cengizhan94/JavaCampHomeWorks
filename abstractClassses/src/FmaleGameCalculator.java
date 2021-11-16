ee4f726c6174827894ddc45a2470f3ae267b84e6
Cengiz Han <90911173+cengizhan94@users.noreply.github.com>
2021-11-16 10:29:18 +0300

    AbstractClasses

---
+++ abstractClassses/src/FmaleGameCalculator.java
@@ -0,0 +1,6 @@
+public class FmaleGameCalculator extends GameCalculator{
+    @Override
+    public void hesapla() {
+
+    }
+}
---
+++ abstractClassses/src/MaleCalculator.java
@@ -0,0 +1,6 @@
+public class MaleCalculator extends GameCalculator{
+    @Override
+    public void hesapla() {
+
+    }
+}
